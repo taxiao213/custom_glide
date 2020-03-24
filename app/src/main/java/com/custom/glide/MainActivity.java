262b945646d5d61eda5429e88a20ebe4eb0a0674
taxiao213 <han13593427958>
2020-03-24 10:28:31 +0800

    Glide缓存项目布局调整

--- app/src/main/java/com/custom/glide/MainActivity.java
+++ app/src/main/java/com/custom/glide/MainActivity.java
@@ -18,8 +18,8 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView iv;
-    //    private String path = "http://img.mp.sohu.com/q_mini,c_zoom,w_640/upload/20170731/4c79a1758a3a4c0c92c26f8e21dbd888_th.jpg";
-    private String path = Environment.getExternalStorageDirectory() + File.separator + "22222.png";
+    private String path = "http://img.mp.sohu.com/q_mini,c_zoom,w_640/upload/20170731/4c79a1758a3a4c0c92c26f8e21dbd888_th.jpg";
+    private String pathFile = Environment.getExternalStorageDirectory() + File.separator + "22222.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Glide.with(MainActivity.this)
                         .load(path)
+                        .into(iv);
+            }
+        });
+        btTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Glide.with(MainActivity.this)
+                        .load(pathFile)
                         .into(iv);
             }
         });
